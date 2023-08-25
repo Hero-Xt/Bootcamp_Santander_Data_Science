7bedb214b76f97639a0290d1579a6a8e4a7c3c97
gabrielsoares <gabriel7mz3256@gmail.com>
2023-08-24 21:22:45 -0300

    Create Tuplas.py

---
+++ Tuplas.py
@@ -0,0 +1,7 @@
+letras = tuple('Python')
+print(letras)
+
+#como é utilizado parenteses para orden de precedencia
+#Se utiliza uma virgula ao final para indeicar que se trata de uma tupla
+frutas = ('Laranja', 'pera', 'uva',)
+print(frutas)
