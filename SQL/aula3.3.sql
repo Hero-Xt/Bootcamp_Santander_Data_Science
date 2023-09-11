ddc7f4ec9da8753022a376e69ba3d25aa8e909ad
gabrielsoares <gabriel7mz3256@gmail.com>
2023-09-10 21:09:01 -0300

    Update aula3.3.sql

--- SQL/aula3.3.sql
+++ SQL/aula3.3.sql
@@ -3,3 +3,9 @@ UPDATE usuarios SET endereco = 'Nova Rua, 123' WHERE email = 'joao@example.com';
 
 -- delete --
 DELETE FROM reservas WHERE status = 'cancelada';
+
+-- Update --
+UPDATE usuarios SET endereco = 'Nova Rua, 123' WHERE email = 'joao@example.com';
+
+-- delete --
+DELETE FROM reservas WHERE status = 'cancelada';
